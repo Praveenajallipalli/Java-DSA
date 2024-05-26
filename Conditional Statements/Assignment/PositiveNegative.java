82602a808b78b559e9e3a4847c6a9df3d3dab1c5
PraveenaJallipalli <jallipallipraveena@gmail.com>
2024-05-26 10:35:05 +0530

    Modified

--- Conditional Statements/Assignment/PositiveNegative.java
+++ Conditional Statements/Assignment/PositiveNegative.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class PositiveNegative {
     public static void main(String[] args) {
-        // Scanner sc=new Scanner(System.in);
-        // int n=sc.nextInt();
-        // if(n>=0) {
-        //     System.out.println("POSITIVE");
-        // }else {
-        //     System.out.println("NEGATIVE");
-        // }
-        // sc.close();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>=0) {
+            System.out.println("POSITIVE");
+        }else {
+            System.out.println("NEGATIVE");
+        }
+        sc.close();
 
         long l=1000;
         float f=l;
