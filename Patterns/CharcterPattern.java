b805f11d036419b2ece0edfb263a48a98e99382b
PraveenaJallipalli <jallipallipraveena@gmail.com>
2024-05-28 04:52:54 +0530

    patterns

--- Patterns/CharcterPattern.java
+++ Patterns/CharcterPattern.java
@@ -4,7 +4,7 @@ public class CharcterPattern {
     public static void main(String[] args) {
         int n=4;
         char ch='A';
-        for(int line=1;line<=4;line++){
+        for(int line=1;line<=n;line++){
             for(int chars=1;chars<=line;chars++){
                 System.out.print(ch);
                 ch++;
